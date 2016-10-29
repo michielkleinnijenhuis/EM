609c5451f019e9d21405588c59765821fe9ea553
Michiel Kleinnijenhuis <michiel.kleinnijenhuis@ndcn.ox.ac.uk>
2016-10-29 19:35:10 +0100

    Writing element size and axislabels in reduceblocks.

--- convert/reduceblocks.py
+++ convert/reduceblocks.py
@@ -24,7 +24,7 @@ def main(argv):
                         help='...')
     parser.add_argument('-f', '--func', default='np.amax',
                         help='...')
-    parser.add_argument('-o', '--outpf', default='_br',
+    parser.add_argument('-o', '--outpf', default='br',
                         help='...')
 
     args = parser.parse_args()
@@ -36,14 +36,38 @@ def main(argv):
     func = args.func
     outpf = args.outpf
 
-    stack, elsize, al = loadh5(datadir, dset_name + inputstack[0],
-                               fieldname=inputstack[1])
+#    stack, elsize, al = loadh5(datadir, dset_name + inputstack[0],
+#                               fieldname=inputstack[1])
 
-    stack = block_reduce(stack, block_size=tuple(blockreduce), func=eval(func))
+    f = filename = os.path.join(datadir, dset_name + inputstack[0] + '.h5')
+    f = h5py.File(filename, 'r')
+    elsize = al = None
+    if 'element_size_um' in f[inputstack[1]].attrs.keys():
+        elsize = f[inputstack[1]].attrs['element_size_um']
+    if 'DIMENSION_LABELS' in f[inputstack[1]].attrs.keys():
+        al = f[inputstack[1]].attrs['DIMENSION_LABELS']
+
+    outsize = [int(np.ceil(d/b)) for d,b in zip(f[inputstack[1]].shape, blockreduce)]
+    print(outsize)
+    
+    filename = os.path.join(datadir, dset_name + outpf + inputstack[0] + '.h5')
+    g = h5py.File(filename, 'w')
+    outds = g.create_dataset(inputstack[1], outsize, dtype=f[inputstack[1]].dtype, compression="gzip")
+
+    outds[:,:,:] = block_reduce(f[inputstack[1]], block_size=tuple(blockreduce), func=eval(func))
     elsize = [e*b for e, b in zip(elsize, blockreduce)]
 
-    writeh5(stack, datadir, dset_name + outpf + inputstack[0],
-            element_size_um=elsize[:3], axislabels=al[:3])
+    if elsize is not None:
+        g[inputstack[1]].attrs['element_size_um'] = elsize
+
+    if al is not None:
+        for i, l in enumerate(al):
+            g[inputstack[1]].dims[i].label = l
+
+    f.close()
+    g.close()
+#    writeh5(stack, datadir, dset_name + outpf + inputstack[0],
+#            element_size_um=elsize[:3], axislabels=al[:3])
 
 
 # ========================================================================== #
