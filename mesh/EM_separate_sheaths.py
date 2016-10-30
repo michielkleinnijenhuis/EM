8c2b3531d9c794ef4dfbd46da1ee028e5a87bce2
Michiel Kleinnijenhuis <michiel.kleinnijenhuis@ndcn.ox.ac.uk>
2016-10-30 09:52:26 +0000

    Argument and readability updates to separate_sheaths.

--- mesh/EM_separate_sheaths.py
+++ mesh/EM_separate_sheaths.py
@@ -13,100 +13,101 @@ from scipy.ndimage import distance_transform_edt
 
 
 def main(argv):
-    
+
     parser = ArgumentParser(description='...')
-    
-    parser.add_argument('datadir', help='...')
-    parser.add_argument('dataset', help='...')
-    parser.add_argument('--maskDS', default=['_maskDS', '/stack'], nargs=2, help='...')
-    parser.add_argument('--maskMM', default=['_maskMM', '/stack'], nargs=2, help='...')
-    parser.add_argument('--maskMA', default=['_maskMA', '/stack'], nargs=2, help='...')
-    parser.add_argument('--supervoxels', default=['_supervoxels', '/stack'], nargs=2, help='...')
-    parser.add_argument('-w', '--sigmoidweighting_MM', action='store_true')
-    parser.add_argument('-d', '--distancefilter_MM', action='store_true')
-    parser.add_argument('-n', '--nzfills', type=int, default=5, 
-                        help='number of characters for section ranges')
-    parser.add_argument('-e', '--element_size_um', type=float, nargs='*', 
-                        default=None, 
-                        help='dataset element sizes (in order of outlayout)')
-    parser.add_argument('-x', default=0, type=int, help='first x-index')
-    parser.add_argument('-X', type=int, help='last x-index')
-    parser.add_argument('-y', default=0, type=int, help='first y-index')
-    parser.add_argument('-Y', type=int, help='last y-index')
-    parser.add_argument('-z', default=0, type=int, help='first z-index')
-    parser.add_argument('-Z', type=int, help='last z-index')
+
+    parser.add_argument('datadir',
+                        help='...')
+    parser.add_argument('dset_name',
+                        help='...')
+    parser.add_argument('-l', '--labelvolume', nargs=2,
+                        default=['_labelMA', 'stack'],
+                        help='...')
+    parser.add_argument('-o', '--outpf', nargs=2,
+                        default=['_labelMM_ws', 'stack'],
+                        help='...')
+    parser.add_argument('--maskDS', nargs=2, default=['_maskDS', 'stack'],
+                        help='...')
+    parser.add_argument('--maskMM', nargs=2, default=['_maskMM', 'stack'],
+                        help='...')
+    parser.add_argument('--maskMA', nargs=2, default=None,
+                        help='...')
+    parser.add_argument('--dist', nargs=2, default=None,
+                        help='...')
+    parser.add_argument('-w', '--sigmoidweighting', action='store_true',
+                        help='...')
+    parser.add_argument('-d', '--distancefilter', action='store_true',
+                        help='...')
 
     args = parser.parse_args()
-    
+
     datadir = args.datadir
-    dataset = args.dataset
+    dset_name = args.dset_name
+    labelvolume = args.labelvolume
+    outpf = args.outpf
     maskDS = args.maskDS
     maskMM = args.maskMM
     maskMA = args.maskMA
-    supervoxels = args.supervoxels
-    sigmoidweighting_MM = args.sigmoidweighting_MM
-    distancefilter_MM = args.distancefilter_MM
-    nzfills = args.nzfills
-    element_size_um = args.element_size_um
-    x = args.x
-    X = args.X
-    y = args.y
-    Y = args.Y
-    z = args.z
-    Z = args.Z
-    
-    dset_info = {'datadir': datadir, 'base': dataset, 
-                 'nzfills': nzfills, 'postfix': '', 
-                 'x': x, 'X': X, 'y': y, 'Y': Y, 'z': z, 'Z': Z}
-    dset_name = dataset_name(dset_info)
+    dist = args.dist
+    sigmoidweighting = args.sigmoidweighting
+    distancefilter = args.distancefilter
 
-    elsize = loadh5(datadir, dset_name)[1]
-    maskDS = loadh5(datadir, dset_name + maskDS[0], 
+    MA, elsize, al = loadh5(datadir, dset_name + labelvolume[0],
+                            fieldname=labelvolume[1])
+    maskDS = loadh5(datadir, dset_name + maskDS[0],
                     fieldname=maskDS[1], dtype='bool')[0]
-    maskMM = loadh5(datadir, dset_name + maskMM[0], 
+    maskMM = loadh5(datadir, dset_name + maskMM[0],
                     fieldname=maskMM[1], dtype='bool')[0]
-    maskMA = loadh5(datadir, dset_name + maskMA[0], 
-                    fieldname=maskMA[1], dtype='bool')[0]
-    svox = loadh5(datadir, dset_name + supervoxels[0], 
-                  fieldname=supervoxels[1])[0]
-
-    if element_size_um is not None:
-        elsize = element_size_um
-    dset_info['elsize'] = elsize
-
-    ### watershed on myelin to separate individual sheaths
-    MA = svox
-    MA[~maskMA] = 0
-    outpf = '_MM'
+    if maskMA is not None:
+        maskMA = loadh5(datadir, dset_name + maskMA[0],
+                        fieldname=maskMA[1], dtype='bool')[0]
+    else:
+        maskMA = MA != 0
 
     # watershed on simple distance transform
-    distance = distance_transform_edt(~maskMA, sampling=np.absolute(elsize))
-    outpf = outpf + '_ws'
-    MM = watershed(distance, grey_dilation(MA, size=(3,3,3)), 
-                   mask=np.logical_and(maskMM, maskDS))
-    writeh5(MM, datadir, dset_name + outpf,
-            element_size_um=elsize, dtype='int32')
+    if dist is not None:
+        distance = loadh5(datadir, dset_name + dist[0],
+                          fieldname=dist[1])[0]
+    else:
+        abs_elsize = np.absolute(elsize)
+        distance = distance_transform_edt(~maskMA, sampling=abs_elsize)
+        sname = dset_name + outpf[0] + '_dist'
+        writeh5(distance, datadir, sname, fieldname=outpf[1],
+                dtype='float', element_size_um=elsize, axislabels=al)
+
+    seeds = grey_dilation(MA, size=(3,3,3))
+    mask = np.logical_and(maskMM, maskDS)
+    MM = watershed(distance, seeds, mask=mask)
+    sname = dset_name + outpf[0] + '_ws'
+    writeh5(MM, datadir, sname, fieldname=outpf[1],
+            dtype='int32', element_size_um=elsize, axislabels=al)
 
     # watershed on sigmoid-modulated distance transform
-    if sigmoidweighting_MM:
-        outpf = outpf + '_sw'
-        distsum, lmask = sigmoid_weighted_distance(MM, MA, elsize)
-        writeh5(distsum, datadir, dset_name + outpf + '_distsum', 
-                element_size_um=elsize, dtype='float')
+    if sigmoidweighting:
+
+        distsum, lmask = sigmoid_weighted_distance(MM, MA, abs_elsize)
+        sname = dset_name + outpf[0] + '_distsum'
+        writeh5(distsum, datadir, sname,
+                dtype='float', element_size_um=elsize, axislabels=al)
+
         MM = watershed(distsum, grey_dilation(MA, size=(3,3,3)), 
                        mask=np.logical_and(maskMM, maskDS))
-        writeh5(MM, datadir, dset_name + outpf,
-                element_size_um=elsize, dtype='int32')
-    else:  # TODO simple distance th
-        lmask = np.zeros((MM.shape[0], MM.shape[1], MM.shape[2], 
-                  len(np.unique(MA)[1:])), dtype='bool')
+        sname = dset_name + outpf[0] + '_sw'
+        writeh5(MM, datadir, sname,
+                dtype='int32', element_size_um=elsize, axislabels=al)
 
-    if distancefilter_MM:  # very mem-intensive
-        outpf = outpf + '_df'
+    elif distancefilter:  # TODO simple distance th
+
+        lmask = np.zeros((MM.shape[0], MM.shape[1], MM.shape[2], 
+                          len(np.unique(MA)[1:])), dtype='bool')
+
+    if distancefilter:  # very mem-intensive
+
         for i,l in enumerate(np.unique(MA)[1:]):
             MM[np.logical_and(lmask[:,:,:,i], MM==l)] = 0
-        writeh5(MM, datadir, dset_name + outpf,
-                element_size_um=elsize, dtype='int32')
+        sname = dset_name + outpf[0] + '_df'
+        writeh5(MM, datadir, sname,
+                dtype='int32', element_size_um=elsize, axislabels=al)
 
 
 # ========================================================================== #
@@ -114,77 +115,96 @@ def main(argv):
 # ========================================================================== #
 
 
-def dataset_name(dname_info):
-    nf = dname_info['nzfills']
-    dname = dname_info['base'] + \
-                '_' + str(dname_info['x']).zfill(nf) + \
-                '-' + str(dname_info['X']).zfill(nf) + \
-                '_' + str(dname_info['y']).zfill(nf) + \
-                '-' + str(dname_info['Y']).zfill(nf) + \
-                '_' + str(dname_info['z']).zfill(nf) + \
-                '-' + str(dname_info['Z']).zfill(nf) + \
-                dname_info['postfix']
-    
-    return dname
-
 def sigmoid_weighted_distance(MM, MA, elsize):
     """"""
+
     lmask = np.zeros((MM.shape[0], MM.shape[1], MM.shape[2], 
                       len(np.unique(MA)[1:])), dtype='bool')
     distsum = np.ones_like(MM, dtype='float')
     medwidth = {}
     for i,l in enumerate(np.unique(MA)[1:]):  # TODO: implement mpi?
         print(i,l)
-        dist = distance_transform_edt(MA!=l, sampling=np.absolute(elsize))
+        dist = distance_transform_edt(MA!=l, sampling=elsize)
         # get the median distance at the outer rim:
         MMfilled = MA + MM
         binim = MMfilled == l
         rim = np.logical_xor(binary_erosion(binim), binim)
         medwidth[l] = np.median(dist[rim])
         # labelmask for voxels further than nmed medians from the object (mem? write to disk?)
-        nmed = 2  # TODO: make into argument
+        nmed = 2  # TODO: make into argument  # to measured in um for low res processing
         maxdist = nmed * medwidth[l]
         lmask[:,:,:,i] = dist > maxdist
         # median width weighted sigmoid transform on distance function
         weighteddist = expit(dist/medwidth[l])  # TODO: create more pronounced transform
         distsum = np.minimum(distsum, weighteddist)
-    
+
     return distsum, lmask
 
+
 def loadh5(datadir, dname, fieldname='stack', dtype=None):
-    """"""
+    """Load a h5 stack."""
 
     f = h5py.File(os.path.join(datadir, dname + '.h5'), 'r')
+
     if len(f[fieldname].shape) == 2:
-        stack = f[fieldname][:,:]
+        stack = f[fieldname][:, :]
     if len(f[fieldname].shape) == 3:
-        stack = f[fieldname][:,:,:]
+        stack = f[fieldname][:, :, :]
     if len(f[fieldname].shape) == 4:
-        stack = f[fieldname][:,:,:,:]
-    if 'element_size_um' in f[fieldname].attrs.keys():
-        element_size_um = f[fieldname].attrs['element_size_um']
-    else:
-        element_size_um = None
+        stack = f[fieldname][:, :, :, :]
+
+    element_size_um, axislabels = get_h5_attributes(f[fieldname])
+
     f.close()
 
     if dtype is not None:
         stack = np.array(stack, dtype=dtype)
 
-    return stack, element_size_um
+    return stack, element_size_um, axislabels
 
-def writeh5(stack, datadir, fp_out, fieldname='stack', dtype='uint16', element_size_um=None):
-    """"""
+
+def writeh5(stack, datadir, fp_out, fieldname='stack',
+            dtype='uint16', element_size_um=None, axislabels=None):
+    """Write a h5 stack."""
+
     g = h5py.File(os.path.join(datadir, fp_out + '.h5'), 'w')
     g.create_dataset(fieldname, stack.shape, dtype=dtype, compression="gzip")
+
     if len(stack.shape) == 2:
-        g[fieldname][:,:] = stack
+        g[fieldname][:, :] = stack
     elif len(stack.shape) == 3:
-        g[fieldname][:,:,:] = stack
+        g[fieldname][:, :, :] = stack
     elif len(stack.shape) == 4:
-        g[fieldname][:,:,:,:] = stack
-    if element_size_um is not None:
-        g[fieldname].attrs['element_size_um'] = element_size_um
+        g[fieldname][:, :, :, :] = stack
+
+    write_h5_attributes(g[fieldname], element_size_um, axislabels)
+
     g.close()
+
+
+def get_h5_attributes(stack):
+    """Get attributes from a stack."""
+
+    element_size_um = axislabels = None
+
+    if 'element_size_um' in stack.attrs.keys():
+        element_size_um = stack.attrs['element_size_um']
+
+    if 'DIMENSION_LABELS' in stack.attrs.keys():
+        axislabels = stack.attrs['DIMENSION_LABELS']
+
+    return element_size_um, axislabels
+
+
+def write_h5_attributes(stack, element_size_um=None, axislabels=None):
+    """Write attributes to a stack."""
+
+    if element_size_um is not None:
+        stack.attrs['element_size_um'] = element_size_um
+
+    if axislabels is not None:
+        for i, l in enumerate(axislabels):
+            stack.dims[i].label = l
 
 
 if __name__ == "__main__":
