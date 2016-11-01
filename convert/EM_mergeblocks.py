91e6d4264aea2389e3f9431e22e5e9af2a2e9485
Michiel Kleinnijenhuis <michiel.kleinnijenhuis@ndcn.ox.ac.uk>
2016-11-01 11:29:02 +0000

    Introduced adapted block_reduce function to work with mode.

--- convert/EM_mergeblocks.py
+++ convert/EM_mergeblocks.py
@@ -7,8 +7,9 @@ import h5py
 import numpy as np
 
 from skimage.segmentation import relabel_sequential
-from skimage.measure import block_reduce
-from scipy.stats.mstats import mode
+# from skimage.measure import block_reduce
+from skimage.util import view_as_blocks
+from scipy.stats import mode
 
 try:
     from mpi4py import MPI
@@ -114,7 +115,6 @@ def main(argv):
         outsize = [int(np.ceil(d/b))
                    for d,b in zip(datasize, blockreduce)]
         elsize = [e*b for e, b in zip(elsize, blockreduce)]
-        print(outsize)
     else:  # NOTE: 'zyx(c)' stack assumed
         outsize = np.subtract(fullsize[::-1], blockoffset[::-1])
         if ndims == 4:
@@ -180,7 +180,8 @@ def main(argv):
             f.close()
             continue
 
-        if not is_labelimage:
+        if ((not is_labelimage) or 
+            (not relabel and not neighbourmerge)):
             gstack[z:Z, y:Y, x:X] = data[oz:oZ, oy:oY, ox:oX]
             f.close()
             continue
@@ -490,6 +491,87 @@ def write_h5_attributes(stack, element_size_um=None, axislabels=None):
     if axislabels is not None:
         for i, l in enumerate(axislabels):
             stack.dims[i].label = l
+
+
+# adapted version of scikit-image-dev0.13 block_reduce
+# it switches to flattened blocks to calculate the (scipy) mode
+def block_reduce(image, block_size, func=np.sum, cval=0):
+    """Down-sample image by applying function to local blocks.
+    Parameters
+    ----------
+    image : ndarray
+        N-dimensional input image.
+    block_size : array_like
+        Array containing down-sampling integer factor along each axis.
+    func : callable
+        Function object which is used to calculate the return value for each
+        local block. This function must implement an ``axis`` parameter such
+        as ``numpy.sum`` or ``numpy.min``.
+    cval : float
+        Constant padding value if image is not perfectly divisible by the
+        block size.
+    Returns
+    -------
+    image : ndarray
+        Down-sampled image with same number of dimensions as input image.
+    Examples
+    --------
+    >>> from skimage.measure import block_reduce
+    >>> image = np.arange(3*3*4).reshape(3, 3, 4)
+    >>> image # doctest: +NORMALIZE_WHITESPACE
+    array([[[ 0,  1,  2,  3],
+            [ 4,  5,  6,  7],
+            [ 8,  9, 10, 11]],
+           [[12, 13, 14, 15],
+            [16, 17, 18, 19],
+            [20, 21, 22, 23]],
+           [[24, 25, 26, 27],
+            [28, 29, 30, 31],
+            [32, 33, 34, 35]]])
+    >>> block_reduce(image, block_size=(3, 3, 1), func=np.mean)
+    array([[[ 16.,  17.,  18.,  19.]]])
+    >>> image_max1 = block_reduce(image, block_size=(1, 3, 4), func=np.max)
+    >>> image_max1 # doctest: +NORMALIZE_WHITESPACE
+    array([[[11]],
+           [[23]],
+           [[35]]])
+    >>> image_max2 = block_reduce(image, block_size=(3, 1, 4), func=np.max)
+    >>> image_max2 # doctest: +NORMALIZE_WHITESPACE
+    array([[[27],
+            [31],
+            [35]]])
+    """
+
+    if len(block_size) != image.ndim:
+        raise ValueError("`block_size` must have the same length "
+                         "as `image.shape`.")
+
+    pad_width = []
+    for i in range(len(block_size)):
+        if block_size[i] < 1:
+            raise ValueError("Down-sampling factors must be >= 1. Use "
+                             "`skimage.transform.resize` to up-sample an "
+                             "image.")
+        if image.shape[i] % block_size[i] != 0:
+            after_width = block_size[i] - (image.shape[i] % block_size[i])
+        else:
+            after_width = 0
+        pad_width.append((0, after_width))
+
+    image = np.pad(image, pad_width=pad_width, mode='constant',
+                   constant_values=cval)
+
+    out = view_as_blocks(image, block_size)
+
+    if func is mode:
+        ndims = len(out.shape) // 2
+        out = np.reshape(out, list(out.shape[:ndims]) + [-1])
+        out = np.squeeze(func(out, axis=-1)[0], axis=-1)
+    else:
+        for i in range(len(out.shape) // 2):
+            out = func(out, axis=-1)
+
+    return out
 
 
 if __name__ == "__main__":
