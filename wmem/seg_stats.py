51e7117f2bcc7ea2d3b2d3bf851dffcdb1021766
Michiel Kleinnijenhuis <michiel.kleinnijenhuis@ndcn.ox.ac.uk>
2017-10-30 14:38:50 +0000

    Segementation stats added to wmem library.

--- wmem/seg_stats.py
+++ wmem/seg_stats.py
@@ -1,59 +1,83 @@
 #!/usr/bin/env python
 
+"""Compute segmentation statistics.
+
+"""
+
 import os
 import sys
-from argparse import ArgumentParser
+import argparse
 
 import h5py
 import numpy as np
 
 from skimage.measure import regionprops
 
+from wmem import parse, utils
+
 
 def main(argv):
+    """Compute segmentation statistics."""
 
-    parser = ArgumentParser(description='...')
-
-    parser.add_argument('datadir',
-                        help='...')
-    parser.add_argument('dset_name',
-                        help='...')
-    parser.add_argument('--labelMA', nargs=2, default=[],
-                        help='...')
-    parser.add_argument('--labelMF', nargs=2, default=[],
-                        help='...')
-    parser.add_argument('--labelUA', nargs=2, default=[],
-                        help='...')
-    parser.add_argument('--stats', nargs='*', default=[],
-                        help='...')
-
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        )
+    parser = parse.parse_seg_stats(parser)
+    parser = parse.parse_common(parser)
     args = parser.parse_args()
 
-    datadir = args.datadir
-    dset_name = args.dset_name
-    labelMA = args.labelMA
-    labelMF = args.labelMF
-    labelUA = args.labelUA
-    stats = args.stats
+    seg_stats(
+        args.h5path_labelMA,
+        args.h5path_labelMF,
+        args.h5path_labelUA,
+        args.stats,
+        args.outputbasename,
+        args.protective,
+        )
 
+
+def seg_stats(
+        labelMA='',
+        labelMF='',
+        labelUA='',
+        stats=['area',
+               'AD',
+               'centroid',
+               'eccentricity',
+               'solidity'],
+        outputbasename='',
+        protective=False,
+        ):
 
     if labelUA:
         name = 'UA'
-        UAprops = get_fibre_stats(datadir, dset_name, labelUA, name, stats)
-        save_stats(datadir, name, UAprops)
+        UAprops = get_fibre_stats(labelUA, stats)
+        if not outputbasename:
+            outbase = labelUA.split('.h5')[0] + '_stats_UA_'
+        save_stats(outbase, UAprops)
 
     if labelMA:
         name = 'MA'
-        MAprops = get_fibre_stats(datadir, dset_name, labelMA, name, stats)
-        save_stats(datadir, name, MAprops)
+        MAprops = get_fibre_stats(labelMA, stats)
+        if not outputbasename:
+            outbase = labelMA.split('.h5')[0] + '_stats_MA_'
+        save_stats(outbase, MAprops)
         MA_area = MAprops['area']
 
     if labelMF:
         name = 'MF'
-        MFprops = get_fibre_stats(datadir, dset_name, labelMF, name, stats)
+        MFprops = get_fibre_stats(labelMF, stats)
+        if not outputbasename:
+            outbase = labelMF.split('.h5')[0] + '_stats_MF_'
+        save_stats(outbase, MFprops)
         MF_area = MFprops['area']
 
     if labelMA and labelMF:
+
+        if not outputbasename:
+            outbase = labelMF.split('.h5')[0] + '_stats_GR_'
+
         # per-axon g-ratios
         gratios = np.empty(MA_area.shape[1])
         gratios[:] = np.NAN
@@ -64,7 +88,8 @@ def main(argv):
 
         MM_area_tot = MF_area_tot[mask] - MA_area_tot[mask]
         gratios = np.sqrt(1 - MM_area_tot / MF_area_tot[mask])
-        np.savetxt(os.path.join(datadir, 'stats_gratios.txt'), gratios[mask])
+        fname = outbase + 'gratios.txt'
+        np.savetxt(fname, gratios[mask])
 
         # slicewise g-ratios
         gratios = np.copy(MF_area)
@@ -73,46 +98,42 @@ def main(argv):
             for j in range(0, MA_area.shape[1]):
                 try:
                     MM_area = MF_area[i, j] - MA_area[i, j]
-                    gratios[i, j] = np.sqrt(1 - float(MM_area) / 
+                    gratios[i, j] = np.sqrt(1 - float(MM_area) /
                                             float(MF_area[i, j]))
                 except:
                     pass
-        fname = os.path.join(datadir, 'stats_gratios_slcws.txt')
+        fname = outbase + 'gratios_slcws.txt'
         np.savetxt(fname, np.array(gratios))
 
     # TODO: use the mask as in filter_NoR
 
-
-# ========================================================================== #
-# function defs
-# ========================================================================== #
+    return
 
 
-def get_fibre_stats(datadir, dset_name, labelvolume, name, stats=[]):
+def get_fibre_stats(h5path_in, stats=[]):
+    """Calculate statistics."""
 
-    LV = loadh5(datadir, dset_name + labelvolume[0],
-                fieldname=labelvolume[1])[0]
+    ds_in = utils.h5_load(h5path_in, load_data=True)[0]
 
-    ulabels = np.unique(LV)
-    Nlabels = len(ulabels) -1
-    print("number of %s labels: %d" % (name, Nlabels))
+    ulabels = np.unique(ds_in)
+    Nlabels = len(ulabels) - 1
+#     print("number of %s labels: %d" % (name, Nlabels))
 
     props = {}
-    props['area'] = np.empty([LV.shape[0], Nlabels], dtype='float')
+    props['area'] = np.empty([ds_in.shape[0], Nlabels], dtype='float')
     props['area'][:, :] = np.NAN
     if 'AD' in stats:
         props['AD'] = np.copy(props['area'])
     if 'centroid' in stats:
-        props['centroid'] = np.empty([LV.shape[0], Nlabels, 2], dtype='float')
+        props['centroid'] = np.empty([ds_in.shape[0], Nlabels, 2], dtype='float')
         props['centroid'][:, :, :] = np.NAN
     if 'eccentricity' in stats:
         props['eccentricity'] = np.copy(props['area'])
     if 'solidity' in stats:
         props['solidity'] = np.copy(props['area'])
 
-    for i in range(0, LV.shape[0]):
-        print(i)
-        rp = regionprops(LV[i,:,:])
+    for i in range(0, ds_in.shape[0]):
+        rp = regionprops(ds_in[i, :, :])
         areas = {prop.label: prop.area for prop in rp}
         if 'AD' in stats:
             eqdia = {prop.label: prop.equivalent_diameter for prop in rp}
@@ -149,95 +170,28 @@ def get_fibre_stats(datadir, dset_name, labelvolume, name, stats=[]):
                 except:
                     pass
 
-    save_stats(datadir, name, props)
-
     return props
 
 
-def save_stats(datadir, name, props):
+def save_stats(outputbasename, props):
+    """Save statistics."""
 
-    fname = os.path.join(datadir, 'stats_%s_slcws_area.txt' % name)
+    fname = outputbasename + 'area.txt'
     np.savetxt(fname, np.array(props['area']))
     if 'AD' in props.keys():
-        fname = os.path.join(datadir, 'stats_%s_slcws_AD.txt' % name)
+        fname = outputbasename + 'AD.txt'
         np.savetxt(fname, np.array(props['AD']))
     if 'centroid' in props.keys():
-        fname = os.path.join(datadir, 'stats_%s_slcws_centroid_y.txt' % name)
-        np.savetxt(fname, np.array(props['centroid'][:,:,0]))
-        fname = os.path.join(datadir, 'stats_%s_slcws_centroid_x.txt' % name)
-        np.savetxt(fname, np.array(props['centroid'][:,:,1]))
+        fname = outputbasename + 'slcws_centroid_y.txt'
+        np.savetxt(fname, np.array(props['centroid'][:, :, 0]))
+        fname = outputbasename + 'slcws_centroid_x.txt'
+        np.savetxt(fname, np.array(props['centroid'][:, :, 1]))
     if 'eccentricity' in props.keys():
-        fname = os.path.join(datadir, 'stats_%s_slcws_eccentricity.txt' % name)
+        fname = outputbasename + 'slcws_eccentricity.txt'
         np.savetxt(fname, np.array(props['eccentricity']))
     if 'solidity' in props.keys():
-        fname = os.path.join(datadir, 'stats_%s_slcws_solidity.txt' % name)
+        fname = outputbasename + 'slcws_solidity.txt'
         np.savetxt(fname, np.array(props['solidity']))
-
-
-def loadh5(datadir, dname, fieldname='stack', dtype=None):
-    """Load a h5 stack."""
-
-    f = h5py.File(os.path.join(datadir, dname + '.h5'), 'r')
-
-    if len(f[fieldname].shape) == 2:
-        stack = f[fieldname][:, :]
-    if len(f[fieldname].shape) == 3:
-        stack = f[fieldname][:, :, :]
-    if len(f[fieldname].shape) == 4:
-        stack = f[fieldname][:, :, :, :]
-
-    element_size_um, axislabels = get_h5_attributes(f[fieldname])
-
-    f.close()
-
-    if dtype is not None:
-        stack = np.array(stack, dtype=dtype)
-
-    return stack, element_size_um, axislabels
-
-
-def writeh5(stack, datadir, fp_out, fieldname='stack',
-            dtype='uint16', element_size_um=None, axislabels=None):
-    """Write a h5 stack."""
-
-    g = h5py.File(os.path.join(datadir, fp_out + '.h5'), 'w')
-    g.create_dataset(fieldname, stack.shape, dtype=dtype, compression="gzip")
-
-    if len(stack.shape) == 2:
-        g[fieldname][:, :] = stack
-    elif len(stack.shape) == 3:
-        g[fieldname][:, :, :] = stack
-    elif len(stack.shape) == 4:
-        g[fieldname][:, :, :, :] = stack
-
-    write_h5_attributes(g[fieldname], element_size_um, axislabels)
-
-    g.close()
-
-
-def get_h5_attributes(stack):
-    """Get attributes from a stack."""
-
-    element_size_um = axislabels = None
-
-    if 'element_size_um' in stack.attrs.keys():
-        element_size_um = stack.attrs['element_size_um']
-
-    if 'DIMENSION_LABELS' in stack.attrs.keys():
-        axislabels = stack.attrs['DIMENSION_LABELS']
-
-    return element_size_um, axislabels
-
-
-def write_h5_attributes(stack, element_size_um=None, axislabels=None):
-    """Write attributes to a stack."""
-
-    if element_size_um is not None:
-        stack.attrs['element_size_um'] = element_size_um
-
-    if axislabels is not None:
-        for i, l in enumerate(axislabels):
-            stack.dims[i].label = l
 
 
 if __name__ == "__main__":
