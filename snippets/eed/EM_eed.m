b5a1a3f81ba7c0ec6f6c5db5028104e4eb5ad274
Michiel Kleinnijenhuis <michiel.kleinnijenhuis@ndcn.ox.ac.uk>
2017-11-07 15:24:47 +0000

    Working version of blockwise EED.

--- snippets/eed/EM_eed.m
+++ snippets/eed/EM_eed.m
@@ -1,18 +1,18 @@
-function u = EM_eed(datadir, invol, ds_in, ds_out, ...
-    start_c, start_z, start_y, start_x, ...
-    count_c, count_z, count_y, count_x, ...
+function ul = EM_eed(datadir, invol, ds_in, ds_out, ...
+    start_z, start_y, start_x, start_c, ...
+    count_z, count_y, count_x, count_c, ...
     margin_z, margin_y, margin_x)
 
 if isdeployed
     % make sure numeric args are numeric
-    start_c = str2double(start_c);
     start_z = str2double(start_z);
     start_y = str2double(start_y);
     start_x = str2double(start_x);
-    count_c = str2double(count_c);
+    start_c = str2double(start_c);
     count_z = str2double(count_z);
     count_y = str2double(count_y);
     count_x = str2double(count_x);
+    count_c = str2double(count_c);
     margin_z = str2double(margin_z);
     margin_y = str2double(margin_y);
     margin_x = str2double(margin_x);
@@ -23,49 +23,52 @@ end
 
 filepath = [datadir filesep invol '.h5'];
 stackinfo = h5info(filepath, ds_in);
+% NOTE: ilastik zyxc is transposed to cxyz;
+% NOTE: axistags not read properly;
 
-margin_z_lower = min([margin_z, start_z - 1])
-margin_y_lower = min([margin_y, start_y - 1])
-margin_x_lower = min([margin_x, start_x - 1])
-start = [start_z - margin_z_lower, ...
-         start_y - margin_y_lower, ...
-         start_x - margin_x_lower]
-
-margin_z_upper = min([...
-    margin_z_lower + start_z + count_z + margin_z, ...
-    stackinfo.Dataspace.Size(1)])
-margin_y_upper = min([...
-    margin_y_lower + start_y + count_y + margin_y, ...
-    stackinfo.Dataspace.Size(2)])
-margin_x_upper = min([...
-    margin_x_lower + start_x + count_x + margin_x, ...
-    stackinfo.Dataspace.Size(3)])
-count = [...
-    margin_z_lower + count_z + margin_z_upper, ...
-    margin_y_lower + count_y + margin_y_upper, ...
-    margin_x_lower + count_x + margin_x_upper]
-
-if start_c == 0
-    data = h5read(filepath, ds_in, start, count);
-else
-    start = [start_c, start_z, start_y, start_x];
-    count = [count_c, count_z, count_y, count_x];
-    data = h5read(filepath, ds_in, start, count);
-    data = squeeze(data(1,:,:,:));
+% create outputfile
+outpath = [datadir filesep invol '_eed.h5'];
+try
+h5create(outpath, ds_out, stackinfo.Dataspace.Size, ...
+    'Deflate', 4, ...
+    'Chunksize', stackinfo.ChunkSize);
+catch
 end
 
-u = CoherenceFilter(data, ...
-    struct('T', 50, 'dt', 1, 'rho', 1, ...
-    'Scheme', 'R', 'eigenmode', 2, 'verbose', 'full'));
+% determine start indices
+margin_z_lower = min([margin_z, start_z - 1]);
+margin_y_lower = min([margin_y, start_y - 1]);
+margin_x_lower = min([margin_x, start_x - 1]);
+start = [start_z - margin_z_lower, ...
+         start_y - margin_y_lower, ...
+         start_x - margin_x_lower];
 
-% fname = [datadir filesep invol num2str(start_c - 1) '_eed2.h5'];
-% h5create(fname, outfield, size(u), 'Deflate', 4, ...
-%     'Chunksize', stackinfo.ChunkSize(2:4));
+% determine counts
+count = [...
+    margin_z_lower + count_z + margin_z, ...
+    margin_y_lower + count_y + margin_y, ...
+    margin_x_lower + count_x + margin_x];
+count(1) = min([count(1), stackinfo.Dataspace.Size(4) - start_z + 1]);
+count(2) = min([count(2), stackinfo.Dataspace.Size(3) - start_y + 1]);
+count(3) = min([count(3), stackinfo.Dataspace.Size(2) - start_x + 1]);
 
-u = u(margin_z + 1:count_z, ...
-      margin_y + 1:count_y, ...
-      margin_x + 1:count_x);
+% read data
+start = [start_c, start(3), start(2), start(1)];
+count = [count_c, count(3), count(2), count(1)];
+data = h5read(filepath, ds_in, start, count);
 
-h5write(filepath, ds_out, u, ...
-    [start_c, start_z, start_y, start_x], ...
-    [count_c, count_z, count_y, count_x]);
+% calculate EED
+ul = zeros([count_c, count_x, count_y, count_z]);
+for c = start_c : start_c + count_c - 1
+    u = CoherenceFilter(squeeze(data(c,:,:,:)), ...
+        struct('T', 50, 'dt', 1, 'rho', 1, ...
+        'Scheme', 'R', 'eigenmode', 2, 'verbose', 'full'));
+    ul(c,:,:,:) = u(margin_x_lower + 1 : count_x + margin_x_lower, ...
+          margin_y_lower + 1 : count_y + margin_y_lower, ...
+          margin_z_lower + 1 : count_z + margin_z_lower);
+end
+
+% write to file
+start_out = [start_c, start_x, start_y, start_z];
+count_out = [count_c, count_x, count_y, count_z];
+h5write(outpath, ds_out, ul, start_out, count_out);
