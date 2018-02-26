55f7cdceaddbfd4d29ea3997ae85366bd27e03e5
Michiel Kleinnijenhuis <michiel.kleinnijenhuis@ndcn.ox.ac.uk>
2018-02-26 08:48:07 +0000

    Updated building instructions for h5py-parallel (not working on ARC).

--- snippets/EM/install/parallel.sh
+++ snippets/EM/install/parallel.sh
@@ -20,29 +20,33 @@ export CONDA_PATH="$(conda info --root)"
 
 module load hdf5-parallel/1.8.17_mvapich2_gcc
 h5pcc -showconfig
+alias mpicc=/system/software/arcus-b/lib/mpi/mvapich2/2.1.0/gcc-4.9.2/bin/mpicc
 export PATH=${DATA}/anaconda2/bin:$PATH
 export CONDA_PATH="$(conda info --root)"
 conda create --name parallel python=2.7
-source activate parallel
-# alias mpicc=/system/software/arcus-b/lib/mpi/mvapich2/2.1.0/gcc-4.9.2/bin/mpicc
 
+source activate parallel
+
+# rsync -Pazv /Users/michielk/Downloads/mpi4py-2.0.0.tar.gz ndcn0180@arcus-b.arc.ox.ac.uk:/home/ndcn-fmrib-water-brain/ndcn0180/workspace
+# cd ~/workspace/mpi4py-2.0.0
 # cd ~/workspace
 # git clone https://github.com/mpi4py/mpi4py.git
 cd ~/workspace/mpi4py
-# rsync -Pazv /Users/michielk/Downloads/mpi4py-2.0.0.tar.gz ndcn0180@arcus-b.arc.ox.ac.uk:/home/ndcn-fmrib-water-brain/ndcn0180/workspace
-# cd ~/workspace/mpi4py-2.0.0
-python setup.py build # --mpicc=/system/software/arcus-b/lib/mpi/mvapich2/2.1.0/gcc-4.9.2/bin/mpicc
-python setup.py install --prefix=/data/ndcn-fmrib-water-brain/ndcn0180/anaconda2/envs/parallel
+python setup.py build
+python setup.py build --mpicc=/system/software/arcus-b/lib/mpi/mvapich2/2.1.0/gcc-4.9.2/bin/mpicc
+python setup.py install --prefix=$CONDA_PREFIX
 
-cd ~/workspace
-git clone https://github.com/h5py/h5py.git
+# conda install -c spectraldns hdf5-parallel
+# h5pcc -showconfig
+
+# cd ~/workspace
+# git clone https://github.com/h5py/h5py.git
 cd ~/workspace/h5py
 export CC=mpicc
 python setup.py configure --mpi --hdf5=/system/software/arcus-b/lib/hdf5/1.8.17/mvapich2-2.1.0__gcc-4.9.2
 python setup.py build
-python setup.py install --prefix=/data/ndcn-fmrib-water-brain/ndcn0180/anaconda2/envs/parallel
-
-
+python setup.py install --prefix=$CONDA_PREFIX
+# pip install . --prefix=$CONDA_PREFIX
 
 
 
@@ -52,18 +56,41 @@ python setup.py install --prefix=/data/ndcn-fmrib-water-brain/ndcn0180/anaconda2
 # export HDF5_MPI="ON"
 # pip install --no-binary=h5py h5py
 
+conda install unittest2
+
+cd $DATA
+testfile='test_mpi4py.py'
+echo "import os" > $testfile
+echo "import h5py" >> $testfile
+echo "from mpi4py import MPI" >> $testfile
+echo "datadir = '/data/ndcn-fmrib-water-brain/ndcn0180/EM/Myrf_01/SET-B/B-NT-S9-2a'" >> $testfile
+echo "h5path = os.path.join(datadir, 'B-NT-S9-2a.h5')" >> $testfile
+echo "h5file = h5py.File(h5path, 'r', driver='mpio', comm=MPI.COMM_WORLD)" >> $testfile
+echo "h5file.close()" >> $testfile
+
+python test.py
 
 
 python
 
+import os
 import h5py
 from mpi4py import MPI
-h5path_file = '/data/ndcn-fmrib-water-brain/ndcn0180/EM/Myrf_01/SET-B/B-NT-S9-2a/B-NT-S9-2a.h5'
-h5file = h5py.File(h5path_file, 'r', driver='mpio', comm=MPI.COMM_WORLD)
+datadir = '/data/ndcn-fmrib-water-brain/ndcn0180/EM/Myrf_01/SET-B/B-NT-S9-2a'
+h5path = os.path.join(datadir, 'B-NT-S9-2a.h5')
+# h5file = h5py.File(h5path, 'r')
+h5file = h5py.File(h5path, 'r', driver='mpio', comm=MPI.COMM_WORLD)
 h5file.close()
 
 
+import os
+import h5py
+from mpi4py import MPI
+datadir = "/Users/michielk/oxdata/P01/EM/Myrf_01/SET-B/B-NT-S10-2f_ROI_00"
+h5path = os.path.join(datadir, 'B-NT-S10-2f_ROI_00ds7_labels.h5')
+# h5file = h5py.File(h5path, 'r')
+h5file = h5py.File(h5path, 'r', driver='mpio', comm=MPI.COMM_WORLD)
+h5file_in.close()
 
-
-python setup.py build --configure
-python setup.py install
+# python setup.py build --configure
+# python setup.py install
