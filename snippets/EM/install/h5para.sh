4eb7167889d06b30073d104670b3226900353afa
Michiel Kleinnijenhuis <michiel.kleinnijenhuis@ndcn.ox.ac.uk>
2018-03-15 17:18:24 +0000

    Setting up parallel hdf5.

--- snippets/EM/install/h5para.sh
+++ snippets/EM/install/h5para.sh
@@ -13,9 +13,29 @@
 
 export PATH=${DATA}/anaconda2/bin:$PATH
 export CONDA_PATH="$(conda info --root)"
-conda create -n h5para
+conda create -n h5para python=2.7
+source activate h5para
 
 conda install h5py-parallel=2.6.0 -c spectralDNS
 conda install hdf5-parallel=1.8.14 -c spectralDNS
 conda install --channel mpi4py mpich mpi4py
 pip install nibabel
+
+
+# jalapeno test failed
+export CONDA_PATH="$(conda info --root)"
+conda create -n h5para python=2.7
+source activate h5para
+conda install h5py-parallel=2.6.0 -c spectralDNS
+
+import os
+import h5py
+from mpi4py import MPI
+datadir = '/data/ndcn-fmrib-water-brain/ndcn0180/EM/Myrf_01/SET-B/B-NT-S9-2a'
+h5path = os.path.join(datadir, 'B-NT-S9-2a.h5')
+# h5file = h5py.File(h5path, 'r')
+h5file = h5py.File(h5path, 'r', driver='mpio', comm=MPI.COMM_WORLD)
+h5file.close()
+
+
+hdf5-parallel/1.8.14_mvapich2
--- snippets/EM/install/parallel.sh
+++ snippets/EM/install/parallel.sh
@@ -1,3 +1,81 @@
+###=========================================================================###
+### NEW BUILDS on arcus-b and jalapeno
+###=========================================================================###
+
+# jalapeno paths
+export DATA=/vols/Data/km/michielk
+export CC=/vols/Data/km/michielk/anaconda2/envs/parallel/bin/mpicc
+export HDF5_PREFIX=/vols/Data/km/michielk/workspace/hdf5
+
+# arcus paths
+export CC=/data/ndcn-fmrib-water-brain/ndcn0180/anaconda2/envs/parallel/bin/mpicc
+export HDF5_PREFIX=/data/ndcn-fmrib-water-brain/ndcn0180/workspace/hdf5
+
+# create conda env
+PATH=${DATA}/anaconda2/bin:$PATH
+CONDA_PATH="$( conda info --root )"
+conda remove --name parallel --all
+conda info --envs
+conda create --name parallel python=2.7 mpi4py unittest2 numpy scipy scikit-image scikit-learn
+source activate parallel
+
+# parallel hdf5 build
+./configure --enable-parallel --prefix=$HDF5_PREFIX
+make
+make check
+make install
+PATH=$HDF5_PREFIX/bin:$PATH
+
+# parallel h5py
+cd ~/workspace
+git clone https://github.com/h5py/h5py.git
+cd h5py
+python setup.py configure --mpi --hdf5=$HDF5_PREFIX
+python setup.py build
+python setup.py install --prefix=$CONDA_PREFIX
+
+# install the wmem package
+cd ~/workspace/EM
+pip install -e .
+
+
+
+###=========================================================================###
+### OLD setup on arcus
+###=========================================================================###
+module load python/2.7 mpi4py/1.3.1 hdf5-parallel/1.8.14_mvapich2
+
+import os
+import h5py
+from mpi4py import MPI
+datadir = "/data/ndcn-fmrib-water-brain/ndcn0180/EM/Myrf_01/SET-B/B-NT-S10-2f_ROI_00"
+h5path_in = os.path.join(datadir, 'blocks_0500', 'B-NT-S10-2f_ROI_00_00000-00520_00000-00520_00000-00184_probs_eed.h5')
+h5dset_in = 'probs_eed'
+h5file_in = h5py.File(h5path_in, 'r+', driver='mpio', comm=MPI.COMM_WORLD)
+ds_in = h5file_in[h5dset_in]
+
+h5path_out = os.path.join(datadir, 'B-NT-S10-2f_ROI_00_probs_eed_newest.h5')
+h5dset_out = 'probs_eed'
+h5file_out = h5py.File(h5path_out, 'a', driver='mpio', comm=MPI.COMM_WORLD)
+# h5ds = h5file_out.create_dataset(h5dset_out, shape=ds_in.shape, dtype=ds_in.dtype, chunks=ds_in.chunks, compression=ds_in.compression)
+h5ds = h5file_out.create_dataset(h5dset_out, shape=ds_in.shape, dtype=ds_in.dtype)
+h5ds[:] = ds_in[:]
+
+h5file_in.close()
+h5file_out.close()
+
+
+
+
+
+
+
+
+
+###=========================================================================###
+### various snippets that failed
+###=========================================================================###
+
 export PATH=${DATA}/anaconda2/bin:$PATH
 export CONDA_PATH="$(conda info --root)"
 # export scriptdir="${HOME}/workspace/EM"
@@ -86,11 +164,11 @@ h5file.close()
 import os
 import h5py
 from mpi4py import MPI
-datadir = "/Users/michielk/oxdata/P01/EM/Myrf_01/SET-B/B-NT-S10-2f_ROI_00"
-h5path = os.path.join(datadir, 'B-NT-S10-2f_ROI_00ds7_labels.h5')
-# h5file = h5py.File(h5path, 'r')
+datadir = "/data/ndcn-fmrib-water-brain/ndcn0180/EM/Myrf_01/SET-B/B-NT-S10-2f_ROI_00"
+h5path = os.path.join(datadir, 'B-NT-S10-2f_ROI_00ds7.h5')
 h5file = h5py.File(h5path, 'r', driver='mpio', comm=MPI.COMM_WORLD)
-h5file_in.close()
+ds_in = h5file['data']
+h5file.close()
 
 # python setup.py build --configure
 # python setup.py install
