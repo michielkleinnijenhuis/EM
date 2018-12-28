170f61146b93f7a3c30bde5e86c3782a367e9e70
Michiel Kleinnijenhuis <michiel.kleinnijenhuis@ndcn.ox.ac.uk>
2018-12-28 12:30:32 +0100

    Updates to testing pipeline.

--- pipelines/EM_pipeline_2018_ARC-JAL-LOC.sh
+++ pipelines/EM_pipeline_2018_ARC-JAL-LOC.sh
@@ -6,23 +6,27 @@ source $scriptdir/pipelines/datasets.sh
 source $scriptdir/pipelines/functions.sh
 source $scriptdir/pipelines/submission.sh
 
-compute_env='JAL'
-compute_env='ARC'
+#compute_env='JAL'
+#compute_env='ARC'
 compute_env='LOCAL'
-compute_env='ARCB'
+#compute_env='ARCB'
 prep_environment $scriptdir $compute_env
 
 # dataset='M3S1GNU'
 # dataset='B-NT-S9-2a'
 # dataset='B-NT-S10-2d_ROI_00'
 # dataset='B-NT-S10-2d_ROI_02'
-# dataset='B-NT-S10-2f_ROI_00'
+dataset='B-NT-S10-2f_ROI_00'
 # dataset='B-NT-S10-2f_ROI_01'
-dataset='B-NT-S10-2f_ROI_02'
+# dataset='B-NT-S10-2f_ROI_02'
 
 bs='0500' && prep_dataset $dataset $bs && echo ${#datastems[@]}
 
-echo -n -e "\033]0;$dataset\007"
+#echo -n -e "\033]0;$dataset\007"
+
+source activate scikit-image-devel_0.13
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
 
 
 ###=========================================================================###
@@ -312,6 +316,26 @@ jid=$( fsl_sub -q veryshort.q $dep $scriptfile )
 # TODO: might want to keep all this in opf='_labels_labelMA_core2D'
 
 
+python $scriptdir/wmem/remap_labels.py \
+$datadir/${dataset_ds}_labels_labelMA_core2D.h5/labelMA_pred_nocore3D \
+$datadir/${dataset_ds}_labels_labelMA_core2D.h5/labelMA_pred_nocore3D_proofread \
+-d $datadir/${dataset_ds}_labels_labelMA_core2D_labelMA_pred_nocore3D_delete.txt
+h52nii '' $dataset_ds '_labels_labelMA_core2D' 'labelMA_pred_nocore3D_proofread' '' '' '-i zyx -o xyz -d uint32'
+
+
+### aggregate 2D labels to 3D labels (merge_slicelabels alternative)
+q=2; o=0.50
+mpiexec -n 6 python $scriptdir/wmem/merge_slicelabels.py -S \
+$datadir/${dataset_ds}_labels_labelMA_core2D_test.h5/labelMA_pred_nocore3D \
+$datadir/${dataset_ds}_labels_labelMA_core2D_test.h5/labelMA_pred_nocore3D_2Dmerge_q${q}-o${o} \
+-M -m 'MAstitch' -d 0 \
+-q $q -o $o -p
+
+h52nii '' $dataset_ds '_labels_labelMA_core2D_test' "labelMA_pred_nocore3D_2Dmerge_q${q}-o${o}" '' '' '-i zyx -o xyz -d uint16'
+h52nii '' $dataset_ds '_labels_labelMA_core2D_test' "labelMA_pred_nocore3D_2Dmerge_q${q}-o${o}_steps/stitched" '' '' '-i zyx -o xyz -d uint16'
+
+
+
 ###=========================================================================###
 ### PROOFREADING / BRUSHING UP myelinated axon compartment
 ###=========================================================================###
@@ -327,15 +351,17 @@ $datadir/${dataset_ds}_labels_labelMA_core3D.h5/labelMA_core3D \
 $datadir/${dataset_ds}_labels_labelMA_core3D.h5/labelMA_core3D_proofread \
 -d $datadir/${dataset_ds}_labels_labelMA_core3D_delete.txt
 h52nii '' $dataset_ds '_labels_labelMA_core3D' 'labelMA_core3D_proofread' '' '' '-i zyx -o xyz -d uint16'
-# # select the labels that do and don't traverse the volume
-# # NOTE: simply replace all labels from the 3Dcore volume with the 2Dcore volume; the tv/nt will be separated later anyway
-# python -W ignore $scriptdir/wmem/nodes_of_ranvier.py -S \
-# $datadir/${dataset_ds}_labels_labelMA_core3D.h5/labelMA_core3D \
-# $datadir/${dataset_ds}_labels_labelMA_core3D_NoR.h5/labelMA_core3D_NoR \
-# --boundarymask $datadir/${dataset_ds}_masks_maskDS.h5/maskDS
-# h52nii '' $dataset_ds '_labels_labelMA_core3D_NoR' "labelMA_core3D_NoR_steps/boundarymask" '' '' '-i zyx -o xyz'
-# h52nii '' $dataset_ds '_labels_labelMA_core3D_NoR' "labelMA_core3D_NoR_steps/labels_tv" '' '' '-i zyx -o xyz -d uint16'
-# h52nii '' $dataset_ds '_labels_labelMA_core3D_NoR' "labelMA_core3D_NoR_steps/labels_nt" '' '' '-i zyx -o xyz -d uint16'
+# # split in the labels that do and don't traverse the volume
+python -W ignore $scriptdir/wmem/nodes_of_ranvier.py -S \
+$datadir/${dataset_ds}_labels_labelMA_core3D.h5/labelMA_core3D \
+$datadir/${dataset_ds}_labels_labelMA_core3D_NoR.h5/labelMA_core3D_NoR \
+--boundarymask $datadir/${dataset_ds}_masks_maskDS.h5/maskDS
+h52nii '' $dataset_ds '_labels_labelMA_core3D_NoR' "labelMA_core3D_NoR_steps/boundarymask" '' '' '-i zyx -o xyz'
+h52nii '' $dataset_ds '_labels_labelMA_core3D_NoR' "labelMA_core3D_NoR_steps/labels_tv" '' '' '-i zyx -o xyz -d uint16'
+h52nii '' $dataset_ds '_labels_labelMA_core3D_NoR' "labelMA_core3D_NoR_steps/labels_nt" '' '' '-i zyx -o xyz -d uint16'
+
+
+
 
 ### masking 3Dlabeled pred with core3D
 # TODO: robust handle mapping of relabeling, for when proofreading requires going back to 2Dcore labels
@@ -352,17 +378,27 @@ h5_dset = 'labelMA_core3D_proofread'
 h5path_in1 = os.path.join(datadir, h5_fname, h5_dset)
 h5file_in1, ds_in1, el, al = utils.h5_load(h5path_in1)
 
-h5_fname = 'B-NT-S10-2f_ROI_00ds7_labels_labelMA.h5'
-h5_dset = 'labelMA_3Dlabeled'
+#h5_fname = 'B-NT-S10-2f_ROI_00ds7_labels_labelMA.h5'
+#h5_dset = 'labelMA_3Dlabeled'
+#h5path_in2 = os.path.join(datadir, h5_fname, h5_dset)
+#h5file_in2, ds_in2, el, al = utils.h5_load(h5path_in2)
+
+#h5_fname = 'B-NT-S10-2f_ROI_00ds7_labels_labelMA_3Dlabeled_filtered.h5'
+#h5_dset = 'labelMA_3Dlabeled_filtered'
+#h5path_out1 = os.path.join(datadir, h5_fname, h5_dset)
+#h5file_out1, ds_out1 = utils.h5_write(None, ds_in2.shape, ds_in2.dtype, h5path_out1, element_size_um=el, axislabels=al)
+
+#h5_dset = 'labelMA_3Dlabeled_filtered_relabeled'
+#h5path_out2 = os.path.join(datadir, h5_fname, h5_dset)
+#h5file_out2, ds_out2 = utils.h5_write(None, ds_in2.shape, ds_in2.dtype, h5path_out2, element_size_um=el, axislabels=al)
+
+h5_fname = 'B-NT-S10-2f_ROI_00ds7_labels_labelMA_core2D_test.h5'
+h5_dset = 'labelMA_pred_nocore3D_2Dmerge_q2-o0.50'
 h5path_in2 = os.path.join(datadir, h5_fname, h5_dset)
 h5file_in2, ds_in2, el, al = utils.h5_load(h5path_in2)
 
-h5_fname = 'B-NT-S10-2f_ROI_00ds7_labels_labelMA_3Dlabeled_filtered.h5'
-h5_dset = 'labelMA_3Dlabeled_filtered'
-h5path_out1 = os.path.join(datadir, h5_fname, h5_dset)
-h5file_out1, ds_out1 = utils.h5_write(None, ds_in2.shape, ds_in2.dtype, h5path_out1, element_size_um=el, axislabels=al)
-
-h5_dset = 'labelMA_3Dlabeled_filtered_relabeled'
+h5_fname = 'B-NT-S10-2f_ROI_00ds7_labels_labelMA_core2D_test.h5'
+h5_dset = 'labelMA_pred_nocore3D_2Dmerge_q2-o0.50_relabeled'
 h5path_out2 = os.path.join(datadir, h5_fname, h5_dset)
 h5file_out2, ds_out2 = utils.h5_write(None, ds_in2.shape, ds_in2.dtype, h5path_out2, element_size_um=el, axislabels=al)
 
@@ -372,28 +408,31 @@ labels_2D = ds_in2[:]
 mask_3D = ds_in1[:].astype('bool')
 maxlabel_3D = np.amax(np.unique(labels_3D))
 labels_2D[mask_3D] = 0
-ds_out1[:] = labels_2D
+#ds_out1[:] = labels_2D
 
 relabels_2D = relabel_sequential(labels_2D, offset=maxlabel_3D+1)[0]
-relabels_2D[mask] = labels_3D[mask_3D]
-ds_out2[:] = labels_2D
+relabels_2D[mask_3D] = 0
+ds_out2[:] = relabels_2D
 
 h5file_in1.close()
 h5file_in2.close()
-h5file_out.close()
+h5file_out1.close()
+h5file_out2.close()
 
-h52nii '' $dataset_ds '_labels_labelMA_3Dlabeled_filtered' 'labelMA_3Dlabeled_filtered' '' '' '-i zyx -o xyz -d uint16'
-h52nii '' $dataset_ds '_labels_labelMA_3Dlabeled_filtered' 'labelMA_3Dlabeled_filtered_relabeled' '' '' '-i zyx -o xyz -d uint16'
+h52nii '' $dataset_ds '_labels_labelMA_core2D_test' 'labelMA_pred_nocore3D_2Dmerge_q2-o0.50_relabeled' '' '' '-i zyx -o xyz -d uint16'
 
+#h52nii '' $dataset_ds '_labels_labelMA_3Dlabeled_filtered' 'labelMA_3Dlabeled_filtered' '' '' '-i zyx -o xyz -d uint16'
+#h52nii '' $dataset_ds '_labels_labelMA_3Dlabeled_filtered' 'labelMA_3Dlabeled_filtered_relabeled' '' '' '-i zyx -o xyz -d uint16'
+
+
+### filtering labels on height
 
 def filter_on_heigth(labels, min_height, ls_short=set([])):
-
     rp_nt = regionprops(labels)
     for prop in rp_nt:
         if prop.bbox[3]-prop.bbox[0] <= min_height:
             ls_short |= set([prop.label])
     print('number of short labels: {}'.format(len(ls_short)))
-
     return ls_short
 
 height = 10
@@ -413,20 +452,21 @@ h5file_in1, ds_in1, el, al = utils.h5_load(h5path_in1)
 
 h5_fname = 'B-NT-S10-2f_ROI_00ds7_labels_labelMA_3Dlabeled_filtered.h5'
 h5_dset = 'labelMA_3Dlabeled_filtered_relabeled'
-h5path_in = os.path.join(datadir, h5_fname, h5_dset)
-h5file_in, ds_in, el, al = utils.h5_load(h5path_in)
+h5path_in2 = os.path.join(datadir, h5_fname, h5_dset)
+h5file_in2, ds_in2, el, al = utils.h5_load(h5path_in2)
 
-h5_dset = 'labelMA_3Dlabeled_filtered_relabeled_sizefilters_h{:02d}'.format(height)
+h5_dset = 'labelMA_pred_nocore3D_2Dmerge_q2-o0.50_relabeled_sizefilters_h{:02d}'.format(height)
+#h5_dset = 'labelMA_3Dlabeled_filtered_relabeled_sizefilters_h{:02d}'.format(height)
 h5path_out = os.path.join(datadir, h5_fname, h5_dset)
-h5file_out, ds_out = utils.h5_write(None, ds_in.shape, ds_in.dtype, h5path_out, element_size_um=el, axislabels=al)
+h5file_out, ds_out = utils.h5_write(None, ds_in2.shape, ds_in2.dtype, h5path_out, element_size_um=el, axislabels=al)
 
 mask_3D = ds_in1[:].astype('bool')
 
-h5root = h5file_in.filename.split('.h5')[0]
+h5root = h5file_in2.filename.split('.h5')[0]
 lsroot = '{}_{}'.format(h5root, ds_out.name[1:])
 
-labels = ds_in[:]
-labels[mask_3D] = 0
+labels = ds_in2[:]
+labels[mask_3D] = 0  # should already be 0, (or: previous step can be skipped but implement the relabeling?)
 ulabels = np.unique(labels)
 maxlabel = np.amax(ulabels)
 labelset = set(ulabels)
@@ -439,23 +479,30 @@ labelset -= ls_short
 fw = np.zeros(maxlabel + 1, dtype='i')
 for l in labelset:
     fw[l] = l
+
 ds_out[:] = fw[labels]
 h5file_out.close()
 
 h5path_out = os.path.join(datadir, h5_fname, h5_dset + '_steps/short')
-h5file_out, ds_out = utils.h5_write(None, ds_in.shape, ds_in.dtype, h5path_out, element_size_um=el, axislabels=al)
+h5file_out, ds_out = utils.h5_write(None, ds_in2.shape, ds_in2.dtype, h5path_out, element_size_um=el, axislabels=al)
 fw = np.zeros(maxlabel + 1, dtype='i')
 for l in ls_short:
     fw[l] = l
+
 ds_out[:] = fw[labels]
 
-h5file_in.close()
+h5file_in1.close()
+h5file_in2.close()
 h5file_out.close()
+
+h52nii '' $dataset_ds '_labels_labelMA_core2D_test' 'labelMA_pred_nocore3D_2Dmerge_q2-o0.50_relabeled_sizefilters_h10' '' '' '-i zyx -o xyz -d uint16'
+h52nii '' $dataset_ds '_labels_labelMA_core2D_test' 'labelMA_pred_nocore3D_2Dmerge_q2-o0.50_relabeled_sizefilters_h10_steps/short' '' '' '-i zyx -o xyz -d uint16'
 
 h52nii '' $dataset_ds '_labels_labelMA_3Dlabeled_filtered' 'labelMA_3Dlabeled_filtered_relabeled_sizefilters_h10' '' '' '-i zyx -o xyz -d uint16'
 h52nii '' $dataset_ds '_labels_labelMA_3Dlabeled_filtered' 'labelMA_3Dlabeled_filtered_relabeled_sizefilters_h10_steps/short' '' '' '-i zyx -o xyz -d uint16'
 
 
+### after proofreading
 python $scriptdir/wmem/remap_labels.py \
 $datadir/${dataset_ds}_labels_labelMA_3Dlabeled_filtered.h5/labelMA_3Dlabeled_filtered_relabeled_sizefilters_h10 \
 $datadir/${dataset_ds}_labels_labelMA_3Dlabeled_filtered.h5/labelMA_3Dlabeled_filtered_relabeled_sizefilters_h10_proofread \
@@ -467,7 +514,214 @@ h52nii '' $dataset_ds '_labels_labelMA_3Dlabeled_filtered' "labelMA_3Dlabeled_fi
 
 
 
+
+
+
+
+### remove outliers from 3Dlabeled
+- size
+- amount of overlap
+- shape
+- centre of mass
+
+
+-- take out core3D_proofread first and --relabel
+-- take out short
+-- per label:
+--- find all 2D labels
+---- load B-NT-S10-2f_ROI_00ds7_labels_labelMA_3Dlabeled_filtered.h5/labelMA_3Dlabeled_filtered_relabeled_noshort as label3D
+---- load B-NT-S10-2f_ROI_00ds7_labels_labelMA_core2D.h5/labelMA_core2D as label2D
+
+
+source activate scikit-image-devel_0.13
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+python
+
+import os
+from wmem import utils
+import numpy as np
+from skimage.measure import regionprops
+
+datadir = '/Users/michielk/oxdata/P01/EM/Myrf_01/SET-B/B-NT-S10-2f_ROI_00'
+
+h5_fname = 'B-NT-S10-2f_ROI_00ds7_labels_labelMA_core3D.h5'
+h5_dset = 'labelMA_core3D_proofread'
+h5path_in0 = os.path.join(datadir, h5_fname, h5_dset)
+h5file_in0, ds_in0, el, al = utils.h5_load(h5path_in0)
+mask = ds_in0[:] > 0
+h5file_in0.close()
+
+h5_fname = 'B-NT-S10-2f_ROI_00ds7_labels_labelMA_core2D.h5'
+h5_dset = 'labelMA_core2D'
+h5path_in1 = os.path.join(datadir, h5_fname, h5_dset)
+h5file_in1, ds_in1, el, al = utils.h5_load(h5path_in1)
+
+h5_fname = 'B-NT-S10-2f_ROI_00ds7_labels_labelMA_3Dlabeled_filtered.h5'
+h5_dset = 'labelMA_3Dlabeled_filtered_relabeled_noshort'
+h5path_in2 = os.path.join(datadir, h5_fname, h5_dset)
+h5file_in2, ds_in2, el, al = utils.h5_load(h5path_in2)
+
+h5_fname = 'B-NT-S10-2f_ROI_00ds7_labels_labelMA.h5'
+h5_dset = 'labelMA_pred'
+h5path_in3 = os.path.join(datadir, h5_fname, h5_dset)
+h5file_in3, ds_in3, el, al = utils.h5_load(h5path_in3)
+
+label2D = ds_in1[:]
+label3D = ds_in2[:]
+pred = ds_in3[:]
+
+label2D[mask] = 0
+label3D[mask] = 0
+pred[mask] = 0
+
+h5_fname = 'B-NT-S10-2f_ROI_00ds7_labels_labelMA_core2D_test.h5'
+h5_dset = 'labelMA_core2D_nocore3D'
+h5path_out = os.path.join(datadir, h5_fname, h5_dset)
+h5file_out, ds_out = utils.h5_write(None, ds_in1.shape, label2D.dtype, h5path_out, element_size_um=el, axislabels=al)
+ds_out[:] = np.squeeze(label2D)
+h5file_out.close()
+h5_fname = 'B-NT-S10-2f_ROI_00ds7_labels_labelMA_core2D_test.h5'
+h5_dset = 'labelMA_3Dlabeled_filtered_relabeled_noshort_nocore3D'
+h5path_out = os.path.join(datadir, h5_fname, h5_dset)
+h5file_out, ds_out = utils.h5_write(None, ds_in2.shape, label3D.dtype, h5path_out, element_size_um=el, axislabels=al)
+ds_out[:] = np.squeeze(label3D)
+h5file_out.close()
+h5_fname = 'B-NT-S10-2f_ROI_00ds7_labels_labelMA_core2D_test.h5'
+h5_dset = 'labelMA_pred_nocore3D'
+h5path_out = os.path.join(datadir, h5_fname, h5_dset)
+h5file_out, ds_out = utils.h5_write(None, ds_in3.shape, pred.dtype, h5path_out, element_size_um=el, axislabels=al)
+ds_out[:] = np.squeeze(pred)
+h5file_out.close()
+
+
+#h52nii '' $dataset_ds '_labels_labelMA_core2D_test' 'labelMA_core2D_nocore3D' '' '' '-i zyx -o xyz -d uint32'
+#h52nii '' $dataset_ds '_labels_labelMA_core2D_test' 'labelMA_3Dlabeled_filtered_relabeled_noshort_nocore3D' '' '' '-i zyx -o xyz -d uint32'
+#h52nii '' $dataset_ds '_labels_labelMA_core2D_test' 'labelMA_pred_nocore3D' '' '' '-i zyx -o xyz -d uint32'
+
+
+# find labelsets from the 2D labelvolume that are contained in the 3D labels
+labelsets = {}
+rp = regionprops(label3D, label2D)
+for prop in rp:
+    print(prop.label)
+    labels2D = np.unique(prop.intensity_image[prop.image])
+    labelsets[prop.label] = set(labels2D)
+
+h5root = 'B-NT-S10-2f_ROI_00ds7_labels_labelMA_3Dlabeled_filtered'
+lsroot = '{}_{}_{}'.format(h5root, h5_dset, '2Dlabels')
+utils.write_labelsets(labelsets, lsroot, ['pickle'])
+
+
+# Load the 2Dprops data.
+nlabels = 794867
+bname = os.path.join(datadir, 'B-NT-S10-2f_ROI_00ds7_labels_labelMA')
+map_propnames = ['label', 'area', 'eccentricity', 'mean_intensity', 'solidity', 'extent', 'euler_number']
+X_train = np.zeros([nlabels, len(map_propnames)])
+for i, propname in enumerate(map_propnames):
+    nppath = '{}_{}.npy'.format(bname, propname)
+    X_train[:, i] = np.load(nppath)  # include the background label
+
+
+# Create a forward map marking outlier labels
+fw = np.zeros([nlabels, 1], dtype='bool')
+#fw = np.zeros([nlabels, 1])
+for lsk, lsv in labelsets.items():
+    #print(lsk)
+    idxs = []
+    feats = np.zeros([len(lsv), len(map_propnames)])
+    for i, slc in enumerate(sorted(lsv)):  # NOTE: sorted relies on increasing labelnr over slices; important for neighbour eval
+        feats[i, :] = X_train[slc, :]
+        #fw[slc] = feats[i, 1]
+        #fw[slc] = True
+    if lsk in set([4759, 20078, 16650, 4892, 7424, 12384]):
+        areafeat = feats[:, 1]
+        area_rel_rsn = np.divide(areafeat, np.roll(areafeat, -1))  # if large, current label is much bigger than right-side neighbour
+        area_rel_rsn[-1] = 0 # idx_max meaningless
+        area_rel_lsn = np.divide(areafeat[::-1], np.roll(areafeat[::-1], -1))   # if large, current label is much bigger than left-side neighbour
+        area_rel_lsn[-1] = 0 # idx_max meaningless
+        thr = 5
+        thr_rsn = area_rel_rsn > thr
+        thr_lsn = area_rel_lsn[::-1] > thr
+        thresholded = thr_rsn | thr_lsn
+        idxs = np.argwhere(thresholded)
+        lsk
+        pp.pprint(zip(areafeat, thresholded)), idxs
+        #med = np.median(areafeat[:, 1])
+        #idxs = np.argwhere(areafeat[:, 1] > 10 * med)  # this just marks the belly of a thickening axon
+        if idxs.any():
+            idxsflat = [idx for idx1 in idxs for idx in idx1]
+            for idx in idxsflat:
+                dellabel = feats[idx, 0]
+                fw[dellabel] = True
+
+mp = fw[ds_in1[:]]
+
+h5_fname = 'B-NT-S10-2f_ROI_00ds7_labels_labelMA_core2D_test.h5'
+h5_dset = 'labelMA_core2D_marked'
+h5path_out = os.path.join(datadir, h5_fname, h5_dset)
+h5file_out, ds_out = utils.h5_write(None, ds_in1.shape, mp.dtype, h5path_out, element_size_um=el, axislabels=al)
+ds_out[:] = np.squeeze(mp)
+h5file_out.close()
+
+h5file_in1.close()
+h5file_in2.close()
+
+h52nii '' $dataset_ds '_labels_labelMA_core2D_test' 'labelMA_core2D_marked' '' '' '-i zyx -o xyz -d uint8'
+
+
+
+
+np.delete(X_train, obj, axis=None)[source]
+
+
+#mask = label3D != label
+#labels2D = np.unique(label2D[mask])
+
+
+
+--- look at 2D label properties
+B-NT-S10-2f_ROI_00ds7_labels_labelMA_area.npy
+...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ### merging interrupted labels  # TODO: make this one step with watershed merge
+source activate scikit-image-devel_0.13
+q=5; o=0.50
+mpiexec -n 6 python $scriptdir/wmem/merge_slicelabels.py -S \
+$datadir/${dataset_ds}_labels_labelMA_3Dlabeled_filtered.h5/labelMA_3Dlabeled_filtered_relabeled_sizefilters_h10_proofread \
+$datadir/${dataset_ds}_labels_labelMA_3Dlabeled_filtered.h5/labelMA_3Dmerge_h10pf_q${q}-o${o} \
+-M -m 'MAstitch' -d 0 \
+-q $q -o $o -p
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+h52nii '' $dataset_ds '_labels_labelMA_3Dlabeled_filtered' "labelMA_3Dmerge_h10pf_q${q}-o${o}" '' '' '-i zyx -o xyz -d uint16'
+h52nii '' $dataset_ds '_labels_labelMA_3Dlabeled_filtered' "labelMA_3Dmerge_h10pf_q${q}-o${o}_steps/stitched" '' '' '-i zyx -o xyz -d uint16'
+
+
+
+
+
+
 # TODO: proofread stitched, make more conservative than -o 0.50, or skip step
 # q=2; o=0.80  # NOTE: no merge results
 q=3; o=0.80
@@ -496,6 +750,8 @@ $datadir/${dataset_ds}_labels_labelMA_3Dlabeled_filtered.h5/labelMA_3Dmerge_q${q
 -d $datadir/${dataset_ds}_labels_labelMA_3Dlabeled_filtered_labelMA_3Dmerge_q3-o0.80_steps-stitched_delete.txt
 h52nii '' $dataset_ds '_labels_labelMA_3Dlabeled_filtered' "labelMA_3Dmerge_q${q}-o${o}_proofread" '' '' '-i zyx -o xyz -d uint16'
 h52nii '' $dataset_ds '_labels_labelMA_3Dlabeled_filtered' "labelMA_3Dmerge_q${q}-o${o}_steps/stitched_proofread" '' '' '-i zyx -o xyz -d uint16'
+
+
 
 
 ### merging interrupted labels
@@ -530,6 +786,9 @@ h52nii '' $dataset_ds '_labels_labelMA_3Dlabeled_filtered' "labelMA_3Dmerge_q${q
 ###=========================================================================###
 ### slicvoxels
 ###=========================================================================###
+# NOTE: SLIC devel LOCAL on 'B-NT-S10-2f_ROI_01'
+# NOTE: SLIC blocks_0500 DONE on ARC for datasets 'M3S1GNU' and 'B-NT-S10-2f_ROI_01'
+# NOTE: these have not been merged or checked
 
 declare l=9000 c=0.20 s=0.03
 declare ipf='_probs' ids='volume/predictions' opf="_slic_slic4D_${l}_${c}_${s}" ods="slic4D_${l}_${c}_${s}"
