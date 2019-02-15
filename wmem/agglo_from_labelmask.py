af0cfc6941628f172023f53e901a5a87157bfb9f
Michiel Kleinnijenhuis <michiel.kleinnijenhuis@ndcn.ox.ac.uk>
2019-02-15 09:30:45 +0100

    WIP labelmask agglomeration.

--- wmem/agglo_from_labelmask.py
+++ wmem/agglo_from_labelmask.py
@@ -54,6 +54,10 @@ def agglo_from_labelmask(
     props = svoxs.get_props(protective=protective)
     mo = LabelImage(outputpath, **props)
     mo.create(comm=mpi.comm)
+#     mo2 = LabelImage(outputpath + '_svoxs', **props)
+#     mo2.create(comm=mpi.comm)
+#     mo3 = LabelImage(outputpath + '_axons', **props)
+#     mo3.create(comm=mpi.comm)
 
     areas_svoxs = np.bincount(svoxs.ds[:].ravel().astype('int64'))
     rp = regionprops(axons.ds, svoxs.ds)
@@ -64,10 +68,19 @@ def agglo_from_labelmask(
 
     for i in mpi.series:
         axon = rp[i]
-#         print(axon.label)
 #         FIXME: double assignments?
         labelset = assign_supervoxels_to_axon(axon, areas_svoxs, ratio_threshold)
-        if labelset:
+
+        # THIS bit was for testing probMA without threshold
+#         svoxs_in_label = axon.intensity_image[axon.image]
+#         areas_svoxs_in_label = np.bincount(svoxs_in_label.astype('int64'))
+#         list_svoxs_in_label = [l for sl in np.argwhere(areas_svoxs_in_label)
+#                                for l in sl]
+#         labelset = set(list_svoxs_in_label) - set([0])
+
+#         if labelset:
+        N_svoxs = 0
+        if len(labelset) > N_svoxs:
             labelsets[axon.label] = labelset
 
     comps = axons.split_path(outputpath)
@@ -77,15 +90,24 @@ def agglo_from_labelmask(
         mpi.comm.Barrier()
 
     if mpi.rank == 0:
-        labelsets = utils.combine_labelsets(labelsets, comps)
-        mo.ds[:] = svoxs.forward_map(labelsets=labelsets, from_empty=True)
-        # TODO: not from_empty, but from axons?
+
+        labelsets = utils.combine_labelsets({}, comps)
+        mo.write(svoxs.forward_map(labelsets=labelsets, from_empty=True))
+
+#         ls_axons = {lskey: set([lskey]) for lskey in labelsets.keys()}
+#         svoxs_fw = svoxs.forward_map(labelsets=labelsets, from_empty=True)
+#         mo2.write(svoxs_fw)
+#         axons_fw = axons.forward_map(labelsets=ls_axons, from_empty=True)
+#         mo3.write(axons_fw)
+#         mo.write(np.maximum(svoxs_fw, axons_fw))
 
     svoxs.close()
     axons.close()
     mo.close()
-
-    return mo
+#     mo2.close()
+#     mo3.close()
+# 
+#     return mo
 
 
 def assign_supervoxels_to_axon(axon, areas_svox, ratio_threshold):
